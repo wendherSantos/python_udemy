b8fe6f15befa42046d1bfbe08850f97b7775535e
wendherSantos <wendher.santos@outlook.com>
2023-05-31 18:53:31 -0300

    udemy

--- iniciante_logica/aula42.py
+++ iniciante_logica/aula42.py
@@ -8,11 +8,13 @@ letra_apareceu_mais_vezes = ''
 
 while i < len(frase):
     letra_atual = frase[i]
-    qtd_apareceu_mais_vezes_atual = frase.count(letra_atual)
 
     if letra_atual == ' ':
         i += 1
         continue
+
+    qtd_apareceu_mais_vezes_atual = frase.count(letra_atual)
+
 
     if qtd_apareceu_mais_vezes < qtd_apareceu_mais_vezes_atual:
         qtd_apareceu_mais_vezes = qtd_apareceu_mais_vezes_atual
