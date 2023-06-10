7323025e3a3bcbd530f576e47f3656446036c57e
wendherSantos <wendher.santos@outlook.com>
2023-06-10 10:26:09 -0300

    udemy

--- iniciante_logica/aula43.py
+++ iniciante_logica/aula43.py
@@ -1,9 +1,28 @@
+# texto = 'Python'
+
+# i = 0
+# tamanho_string = len(texto)
+
+# while i < tamanho_string:
+#     print(texto[i], i + 1)
+#     i += 1
+
+# senha_salva = '123456'
+# senha_digitada = ''
+# repeticoes = 0
+
+# while senha_salva != senha_digitada:
+#     senha_digitada = input(f'Sua senha ({repeticoes}x): ')
+
+#     repeticoes += 1
+
+# print('Aquele laço acima pode ter repetições infinitas')
+
 texto = 'Python'
+novo_texto = ''
 
-i = 0
-tamanho_string = len(texto)
+for letra in texto:
+    novo_texto += f'*{letra}'
+    print(letra)
 
-while i < tamanho_string:
-    print(texto[i], i + 1)
-    i += 1
-
+print(novo_texto + '*')
