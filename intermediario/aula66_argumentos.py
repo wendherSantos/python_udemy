52b70d991d686a06ec9c4cf440fe13ab3f066456
wendherSantos <wendher.santos@outlook.com>
2023-06-14 18:14:27 -0300

    udemy

---
+++ intermediario/aula66_argumentos.py
@@ -0,0 +1,10 @@
+"""
+argumntos nomeados e nao nomeados em funcoes python
+argumento nomeado tem nome com sinal de igual
+argumento nao nomeado recebe apenas o argumento (valor)
+"""
+# definicao
+def soma(x, y):
+    print(x + y)
+    
+soma(1, 2)
